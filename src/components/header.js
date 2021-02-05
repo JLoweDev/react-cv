6b3be4e8c4a6de02703eb6835f247e2e85d7cbe4
James <67014970+JLoweDev@users.noreply.github.com>
2021-02-05 16:55:46 +0000

    changed language to react

--- src/components/header.js
+++ src/components/header.js
@@ -2,9 +2,11 @@ import { FaGithub } from 'react-icons/fa'
 
 const Header = ({ title, github }) => {
     return (
-        <header className='header'>
-            <h1>{title}</h1>
-            <a href={github}><FaGithub /></a>
+        <header className='fixed w-full z-30 top-0 text-black'>
+            <div className='w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2'>
+                <h1 className='text-2xl font-bold'>{title}</h1>
+                <a href={github} className='text-3xl'><FaGithub /></a>
+            </div>
         </header>
     )
 }
