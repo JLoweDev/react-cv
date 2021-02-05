34df91a1d2580e72e364b51276f0bccb0659e911
James <67014970+JLoweDev@users.noreply.github.com>
2021-02-05 17:13:32 +0000

    Simple tailwind header added

--- src/components/header.js
+++ src/components/header.js
@@ -2,61 +2,34 @@ import React, { useState } from 'react'
 import { FaGithub } from 'react-icons/fa'
 
 const Header = ({ title, github }) => {
-    const [navbarOpen, setNavbarOpen] = React.useState(false);
     return (
-        // Creative Tim React Header https://www.creative-tim.com/learning-lab/tailwind-starter-kit/documentation/react/navbars
+        // Tailwind header added from https://tailwindcomponents.com/component/responsive-header
         <header>
-            <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-red-500 mb-3">
-                <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-                <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-                    <a
-                    className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white"
-                    href="#pablo"
-                    >
-                    orange Tailwind Starter Kit
-                    </a>
-                    <button
-                    className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-                    type="button"
-                    onClick={() => setNavbarOpen(!navbarOpen)}
-                    >
-                    <i className="fas fa-bars"></i>
+            <nav class="flex items-center justify-between flex-wrap bg-green-500 p-6">
+                <div class="flex items-center flex-no-shrink text-white mr-6">
+                    <svg class="h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+                    <span class="font-semibold text-xl tracking-tight">Tailwind CSS</span>
+                </div>
+                <div class="block lg:hidden">
+                    <button class="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
+                    <svg class="h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
                     </button>
                 </div>
-                <div
-                    className={
-                    "lg:flex flex-grow items-center" +
-                    (navbarOpen ? " flex" : " hidden")
-                    }
-                    id="example-navbar-danger"
-                >
-                    <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
-                    <li className="nav-item">
-                        <a
-                        className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                        href="#pablo"
-                        >
-                        <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Share</span>
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a
-                        className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                        href="#pablo"
-                        >
-                        <i className="fab fa-twitter text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Tweet</span>
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a
-                        className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                        href="#pablo"
-                        >
-                        <i className="fab fa-pinterest text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Pin</span>
-                        </a>
-                    </li>
-                    </ul>
-                </div>
+                <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+                    <div class="text-sm lg:flex-grow">
+                    <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4">
+                        Docs
+                    </a>
+                    <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4">
+                        Examples
+                    </a>
+                    <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white">
+                        Blog
+                    </a>
+                    </div>
+                    <div>
+                    <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal hover:bg-white mt-4 lg:mt-0">Download</a>
+                    </div>
                 </div>
             </nav>
         </header>
