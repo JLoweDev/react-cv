e35e2cf9ff5850a0d8e1854dfd61809d06b01717
James <67014970+JLoweDev@users.noreply.github.com>
2021-02-06 01:41:19 +0000

    Added a card for general info

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react'
 import Header from './components/Header'
+import GeneralInfo from './components/GeneralInfo'
 import Footer from './components/Footer'
 
 function App() {
   return (
     <div>
       <Header title='React CV App'/>
+      <GeneralInfo />
     </div>
   )
 }
---
+++ src/components/GeneralInfo.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const GeneralInfo = () => {
+    return (
+        <div>
+            <div class="max-w-2xl mx-auto sm:px-6 lg:px-8">
+                <div class="overflow-hidden shadow-md">
+                    <div class="px-6 py-4 bg-white border-b border-gray-200 font-bold uppercase">
+                        What is Lorem Ipsum?
+                    </div>
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                        when an unknown printer took a galley of type and scrambled it to make a type 
+                        specimen book.
+                    </div>
+                    <div class="p-6 bg-white border-gray-200 text-right">
+                        <a class="bg-blue-500 shadow-md text-sm text-white font-bold py-3 md:px-8 px-4 hover:bg-blue-400 rounded uppercase" 
+                            href="#">Click Me</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default GeneralInfo
