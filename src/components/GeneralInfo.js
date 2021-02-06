31c08cfac1d0b3ca4d1ab9856f796bc4d6765a95
James <67014970+JLoweDev@users.noreply.github.com>
2021-02-06 01:55:08 +0000

    styled GeneralInfo card better

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -3,16 +3,22 @@ import React from 'react'
 const GeneralInfo = () => {
     return (
         <div>
-            <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
+            <div class="max-w-sm mx-auto sm:px-6 lg:px-8">
                 <div class="overflow-hidden shadow-md">
                     <div class="mt-1 px-2 py-1 bg-white border-b border-gray-200 font-bold uppercase">
                         General Information
                     </div>
-                    <div class="p-1 bg-white">
-                        <span>Beef</span>
+                    <div class="p-1 bg-white flex flex-row justify-between">
+                        <h1>Name:</h1>
+                        <p>Chicken</p>
                     </div>
-                    <div class="p-1 bg-white border-b border-gray-200">
-                        <span>Pork</span>
+                    <div class="p-1 bg-white flex flex-row justify-between">
+                        <h1>Email:</h1>
+                        <p>Beef</p>
+                    </div>
+                    <div class="p-1 bg-white border-b border-gray-200 flex flex-row justify-between">
+                        <h1>Phone Number:</h1> 
+                        <p>Pork</p>
                     </div>
                     <div class="p-3 bg-white border-gray-200 text-right">
                         <a class="bg-indigo-600 shadow-md text-sm text-white font-bold py-2 md:px-4 px-1 hover:bg-indigo-400 rounded uppercase" 
