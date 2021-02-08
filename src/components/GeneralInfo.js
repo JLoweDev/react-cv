22f72b732b34ed1058b466a1bd94a560e2fec204
James <67014970+JLoweDev@users.noreply.github.com>
2021-02-08 17:06:48 +0000

    replaced class with className for JSX

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -3,25 +3,25 @@ import React from 'react'
 const GeneralInfo = () => {
     return (
         <div>
-            <div class="max-w-sm mx-auto sm:px-6 lg:px-8">
-                <div class="overflow-hidden shadow-md">
-                    <div class="mt-1 px-2 py-1 bg-white border-b border-gray-200 font-bold uppercase">
+            <div className="max-w-sm mx-auto sm:px-6 lg:px-8">
+                <div className="overflow-hidden shadow-md">
+                    <div className="mt-1 px-2 py-1 bg-white border-b border-gray-200 font-bold uppercase">
                         General Information
                     </div>
-                    <div class="p-1 bg-white flex flex-row justify-between">
-                        <h1>Name:</h1>
+                    <div className="p-1 bg-white flex flex-row justify-between">
+                        <h1 className=''>Name:</h1>
                         <p>John Smith</p>
                     </div>
-                    <div class="p-1 bg-white flex flex-row justify-between">
+                    <div className="p-1 bg-white flex flex-row justify-between">
                         <h1>Email:</h1>
                         <p>JohnSmith@gmail.com</p>
                     </div>
-                    <div class="p-1 bg-white border-b border-gray-200 flex flex-row justify-between">
+                    <div className="p-1 bg-white border-b border-gray-200 flex flex-row justify-between">
                         <h1>Phone Number:</h1> 
                         <p>01023734123</p>
                     </div>
-                    <div class="p-3 bg-white border-gray-200 text-right">
-                        <a class="bg-indigo-600 shadow-md text-sm text-white font-bold py-2 md:px-4 px-1 hover:bg-indigo-400 rounded uppercase" 
+                    <div className="p-3 bg-white border-gray-200 text-right">
+                        <a className="bg-indigo-600 shadow-md text-sm text-white font-bold py-2 md:px-4 px-1 hover:bg-indigo-400 rounded uppercase" 
                             href="#">Edit Details</a>
                     </div>
                 </div>
--- src/components/header.js
+++ src/components/header.js
@@ -5,24 +5,24 @@ const Header = ({ title }) => {
     return (
         // Tailwind header added from https://tailwindcomponents.com/component/responsive-header
         <header>
-            <nav class="w-full flex flex-row items-center justify-between bg-indigo-600 p-4">
-                <div class="block text-white mr-6">
-                    <span class="font-bold text-lg tracking-tight">{title}</span>
+            <nav className="w-full flex flex-row items-center justify-between bg-indigo-600 p-4">
+                <div className="block text-white mr-6">
+                    <span className="font-bold text-lg tracking-tight">{title}</span>
                 </div>
                 <div>
-                    <a target='__blank' href='https://github.com/JLoweDev/react-cv' class="inline-block text-sm px-2 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-indigo-600 hover:bg-white mt-2 ml-1 lg:mt-0 lg:ml-2">
+                    <a target='__blank' href='https://github.com/JLoweDev/react-cv' className="inline-block text-sm px-2 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-indigo-600 hover:bg-white mt-2 ml-1 lg:mt-0 lg:ml-2">
                         <FaGithub />
                     </a>
-                    <a target='__blank' href='https://www.youtube.com/channel/UCYkZtYIbyDdb1fo3Pf1Q4rg/featured' class="inline-block text-sm px-2 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-indigo-600 hover:bg-white mt-2 ml-1 lg:mt-0 lg:ml-2">
+                    <a target='__blank' href='https://www.youtube.com/channel/UCYkZtYIbyDdb1fo3Pf1Q4rg/featured' className="inline-block text-sm px-2 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-indigo-600 hover:bg-white mt-2 ml-1 lg:mt-0 lg:ml-2">
                         <FaYoutube />
                     </a>
-                    <a target='__blank' href='https://www.linkedin.com/in/james-lowe-b0b67b162/' class="inline-block text-sm px-2 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-indigo-600 hover:bg-white mt-2 ml-1 lg:mt-0 lg:ml-2">
+                    <a target='__blank' href='https://www.linkedin.com/in/james-lowe-b0b67b162/' className="inline-block text-sm px-2 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-indigo-600 hover:bg-white mt-2 ml-1 lg:mt-0 lg:ml-2">
                         <FaLinkedin />
                     </a>
-                    <a target='__blank' href='https://twitter.com/JLoweDev' class="inline-block text-sm px-2 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-indigo-600 hover:bg-white mt-2 ml-1 lg:mt-0 lg:ml-2">
+                    <a target='__blank' href='https://twitter.com/JLoweDev' className="inline-block text-sm px-2 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-indigo-600 hover:bg-white mt-2 ml-1 lg:mt-0 lg:ml-2">
                         <FaTwitter />
                     </a>
-                    <a target='__blank' href='#' class="inline-block text-sm px-2 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-indigo-600 hover:bg-white mt-2 ml-1 lg:mt-0 lg:ml-2">
+                    <a target='__blank' href='#' className="inline-block text-sm px-2 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-indigo-600 hover:bg-white mt-2 ml-1 lg:mt-0 lg:ml-2">
                         <FaTwitch />
                     </a>
                 </div>
