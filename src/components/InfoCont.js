b69850ebbc485ce830b97b92d0cb41389b61c27b
James <67014970+JLoweDev@users.noreply.github.com>
2021-02-09 16:03:33 +0000

    Practical Info component added

--- src/components/InfoCont.js
+++ src/components/InfoCont.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import GeneralInfo from './GeneralInfo'
 import EduInfo from './EduInfo'
+import PracInfo from './PracInfo'
 
 const InfoCont = () => {
     return (
         <div className='flex flex-row justify-between mt-12 mx-32'>
             <GeneralInfo />
             <EduInfo />
-            <GeneralInfo />
+            <PracInfo />
         </div>
     )
 }
--- src/components/PracInfo.js
+++ src/components/PracInfo.js
@@ -1,28 +1,32 @@
 import React from 'react'
 
-const EduInfo = () => {
+const PracInfo = () => {
     return (
         <div>
             <div className="max-w-md mx-auto sm:px-6 lg:px-8">
                 <div className="overflow-hidden shadow-md">
                     <div className="mt-1 px-2 py-1 bg-white border-b border-gray-200 font-bold uppercase">
-                        Educational Experience
+                        Practical Experience
                     </div>
                     <div className="p-1 bg-white flex flex-row justify-between">
-                        <h1 className='pr-8'>School Name:</h1>
-                        <p>Harvard</p>
+                        <h1 className='pr-8'>Company Name:</h1>
+                        <p>Starbucks</p>
                     </div>
                     <div className="p-1 bg-white flex flex-row justify-between">
-                        <h1 className='pr-8'>Course:</h1>
-                        <p>Computer Science</p>
+                        <h1 className='pr-8'>Position:</h1>
+                        <p>Barista</p>
+                    </div>
+                    <div className="p-1 bg-white flex flex-row justify-between">
+                        <h1 className='pr-8'>Main Tasks:</h1>
+                        <p>Spitting in Coffee</p>
                     </div>
                     <div className="p-1 bg-white flex flex-row justify-between">
                         <h1 className='pr-8'>Date Started:</h1> 
-                        <p>Oct-2016</p>
+                        <p>Jul-2020</p>
                     </div>
                     <div className="p-1 bg-white border-b border-gray-200 flex flex-row justify-between">
                         <h1 className='pr-8'>Date Completed:</h1> 
-                        <p>Jun-2020</p>
+                        <p>Sep-2020</p>
                     </div>
                     <div className='flex flex-row justify-between'>
                         <div className="p-3 bg-white border-gray-200 text-right">
@@ -40,4 +44,4 @@ const EduInfo = () => {
     )
 }
 
-export default EduInfo
+export default PracInfo
