72885dfb5054802dfa30cc3fe95e00efc1560b16
James <67014970+JLoweDev@users.noreply.github.com>
2021-02-09 15:58:18 +0000

    Educational Info Added to Container

--- src/components/EduInfo.js
+++ src/components/EduInfo.js
@@ -3,7 +3,7 @@ import React from 'react'
 const EduInfo = () => {
     return (
         <div>
-            <div className="max-w-sm mx-auto sm:px-6 lg:px-8">
+            <div className="max-w-md mx-auto sm:px-6 lg:px-8">
                 <div className="overflow-hidden shadow-md">
                     <div className="mt-1 px-2 py-1 bg-white border-b border-gray-200 font-bold uppercase">
                         Education
@@ -24,9 +24,15 @@ const EduInfo = () => {
                         <h1 className='pr-8'>Date Completed:</h1> 
                         <p>Jun-2020</p>
                     </div>
-                    <div className="p-3 bg-white border-gray-200 text-right">
-                        <a className="bg-indigo-600 shadow-md text-sm text-white font-bold py-2 md:px-4 px-1 hover:bg-indigo-400 rounded uppercase" 
-                            href="#">Edit Details</a>
+                    <div className='flex flex-row justify-between'>
+                        <div className="p-3 bg-white border-gray-200 text-right">
+                            <a className="bg-indigo-600 shadow-md text-sm text-white font-bold py-2 md:px-4 px-1 hover:bg-indigo-400 rounded uppercase" 
+                                href="#">Add More</a>
+                        </div>
+                        <div className="p-3 bg-white border-gray-200 text-right">
+                            <a className="bg-indigo-600 shadow-md text-sm text-white font-bold py-2 md:px-4 px-1 hover:bg-indigo-400 rounded uppercase" 
+                                href="#">Edit Details</a>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/InfoCont.js
+++ src/components/InfoCont.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import GeneralInfo from './GeneralInfo'
+import EduInfo from './EduInfo'
 
 const InfoCont = () => {
     return (
         <div className='flex flex-row justify-between mt-12 mx-32'>
             <GeneralInfo />
-            <GeneralInfo />
+            <EduInfo />
             <GeneralInfo />
         </div>
     )
